615f65e795fdc89b24c32cec5dcc38eefc9e79c4
jmpaulo89 <jmpaulo89@hotmail.com>
2025-05-31 12:55:01 +0100

    Iniial

---
+++ app.js
@@ -0,0 +1,56 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+const toggles = document.querySelectorAll('.toggle')
+const good = document.getElementById('good')
+const cheap = document.getElementById('cheap')
+const fast = document.getElementById('fast')
+const boxes = document.querySelectorAll('.box');
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => {
+    doTheTrick(e.target)
+
+}))
+
+function doTheTrick(theClickedOne) {
+    if (good.checked && cheap.checked && fast.checked) {
+        if (good === theClickedOne) {
+            fast.checked = false
+        }
+
+        if (cheap === theClickedOne) {
+            good.checked = false
+        }
+        if (fast === theClickedOne) {
+            cheap.checked = false
+        }
+    }
+}
+
+
+function fixNav() {
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes();
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 4 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
